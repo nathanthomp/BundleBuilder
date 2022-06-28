62f6bb089bc3f88e653aa3e63be7c38902032c20
Nathan Thompson <nathanthomp30@gmail.com>
2022-06-28 13:30:41 -0400

    Remove WPF project

---
+++ RedBuilt.Revit.BundleBuilder/ViewModels/ShellViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedBuilt.Revit.BundleBuilder.ViewModels
+{
+    public class ShellViewModel : ViewModelBase
+    {
+        public ViewModelBase CurrentViewModel { get; set; }
+
+    }
+}
