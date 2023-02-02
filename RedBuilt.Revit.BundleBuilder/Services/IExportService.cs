d5fbb1600ab1058980e092fc1962610bc4ea191c
Nathan Thompson <nthompson@redbuilt.com>
2023-02-02 14:57:31 -0500

    Add interface export service

---
+++ RedBuilt.Revit.BundleBuilder/Services/IExportService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedBuilt.Revit.BundleBuilder.Services
+{
+    public interface IExportService
+    {
+        void Export();
+        void Restore();
+    }
+}
